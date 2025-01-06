542de798309883d3c7c4037b4c7cd100bb004453
Dragos Andrei Munteanu <i516586@student.fontys.nl>
2025-01-06 17:22:36 +0200

    Enabling swagger for production to test

---
+++ LoreVault/LoreVault.API/Controllers/SecondHealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoreVault.API.Controllers
+{
+    [ApiController]
+    [Route("Health")]
+    public class SecondHealthCheck : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Health!");
+        }
+    }
+}
